187f53b265df535dff387db3fb54bebde1b665a6
jack <jack.nguyen@weareday.one>
2023-02-16 14:20:41 +0700

    chores: use onSnapshot for category && article

--- src/hooks/useArticle.js
+++ src/hooks/useArticle.js
@@ -1,26 +1,27 @@
 import { useState, useEffect } from 'react';
-import { query, collection, orderBy, getDocs } from 'firebase/firestore';
+import { query, collection, orderBy } from 'firebase/firestore';
 
 import { firestore } from '../configs/firebase.config';
 
 const useArticle = () => {
   const [articles, setArticles] = useState([]);
 
-  const getArticles = async () => {
-    try {
-      const q = query(
-        collection(firestore, 'articles'),
-        orderBy('createdAt', 'desc')
-      );
-      const snapshot = await getDocs(q);
-      setArticles(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
   useEffect(() => {
-    getArticles();
+    const q = query(
+      collection(firestore, 'articles'),
+      orderBy('createdAt', 'desc')
+    );
+
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const docs = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      setArticles(docs);
+    });
+
+    return unsubscribe;
   }, []);
 
   return {
--- src/hooks/useCategory.js
+++ src/hooks/useCategory.js
@@ -1,29 +1,27 @@
 import { useState, useEffect } from 'react';
-import { query, collection, orderBy, getDocs } from 'firebase/firestore';
+import { query, collection, orderBy } from 'firebase/firestore';
 
 import { firestore } from '../configs/firebase.config';
 
 const useCategory = () => {
   const [categories, setCategories] = useState([]);
 
-  const getCategories = async () => {
-    try {
-      const q = query(
-        collection(firestore, 'categories'),
-        orderBy('order', 'asc')
-      );
-      const snapshot = await getDocs(q);
-      setCategories([
-        { id: '1', enName: 'Latest', viName: 'Mới nhất' },
-        ...snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })),
-      ]);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
   useEffect(() => {
-    getCategories();
+    const q = query(
+      collection(firestore, 'categories'),
+      orderBy('order', 'asc')
+    );
+
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const docs = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      setCategories(docs);
+    });
+
+    return unsubscribe;
   }, []);
 
   return { categories };
